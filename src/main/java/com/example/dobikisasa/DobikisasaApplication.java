75ddd050842d2db03f1f1c50bf36cfd1ea07e07d
Elison Nyagwaru <elianitunga@gmail.com>
2024-05-06 12:02:27 +0300

    first commit

---
+++ src/main/java/com/example/dobikisasa/DobikisasaApplication.java
@@ -0,0 +1,13 @@
+package com.example.dobikisasa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DobikisasaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DobikisasaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/dobikisasa/DobikisasaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.dobikisasa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DobikisasaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
